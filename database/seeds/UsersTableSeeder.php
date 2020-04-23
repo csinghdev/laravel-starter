1f5a0f8f27939a92aff0887d2388ab243da6df4d
Chandresh <chandresh989@gmail.com>
2020-04-23 22:41:19 +0530

    Users table seeder

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+            'name' => 'John Doe',
+            'email' => 'john@example.com',
+            'password' => 'test'
+        ]);
+    }
+}
